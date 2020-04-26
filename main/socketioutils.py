115bf8494f1232f7273a53ebcc75d2ab8dce2529
Sugarbabyzz <406857586@qq.com>
2020-04-26 10:32:02 +0800

    0426_修复掷骰子bug
    1、修复掷骰子只能在自己界面显示，其他人无法看到的bug（不能仅前端处理，需要走服务器）

--- main/socketioutils.py
+++ main/socketioutils.py
@@ -10,12 +10,27 @@ from flask import render_template
 @socketio.on('new_message')
 def new_message(content):
     """ 消息发送 """
-    print(content)
     message = models.Message(author=current_user._get_current_object(), content=clean_html(content))
     db.session.add(message)
     db.session.commit()
     emit('new_message',
          {
              'message_html': render_template('message.html', message=message)
+         },
+         broadcast=True)
+
+
+@socketio.on('dice_message')
+def dice_message(dice_message_json):
+    """ 掷骰子请求 """
+    content = str(current_user._get_current_object().username) + ' 掷出了 ' + str(dice_message_json['result_number']) + \
+              '（范围 1~' + str(dice_message_json['max_number']) + '）'
+    user_system = db.session.query(models.User).filter(db.and_(models.User.username == '系统')).first()
+    message = models.Message(author=user_system, content=content)
+    db.session.add(message)
+    db.session.commit()
+    emit('dice_message',
+         {
+             'dice_message_html': render_template('message_dice.html', message=message)
          },
          broadcast=True)
